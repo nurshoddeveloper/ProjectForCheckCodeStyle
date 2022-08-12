d88c3a9d96a6d198c0e089f222554f6ec997630e
nurwod0118 <Nurwod0118@mail.ru>
2022-08-12 14:53:28 +0500

    fixed

---
+++ Todo-master/frontend/build/precache-manifest.5708d4452f64742e742206d7897f01dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a17bfa647129349040efc551ef3ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa67a94a9e60a91db16a",
+    "url": "/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "4d0491fb1e6ce6671fd4",
+    "url": "/static/css/main.3ba987c5.chunk.css"
+  },
+  {
+    "revision": "aa67a94a9e60a91db16a",
+    "url": "/static/js/2.8ecdc574.chunk.js"
+  },
+  {
+    "revision": "27f1c73f1dc921bc79aa41830d4cd146",
+    "url": "/static/js/2.8ecdc574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0491fb1e6ce6671fd4",
+    "url": "/static/js/main.87036e12.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/static/media/not_found.6695af99.svg"
+  },
+  {
+    "revision": "9655f6c7278c4bf21b320ec931ad2749",
+    "url": "/static/media/send_message.9655f6c7.png"
+  },
+  {
+    "revision": "3fc12c943c101b7815f7c638c5441860",
+    "url": "/static/media/success.3fc12c94.png"
+  }
+]);
